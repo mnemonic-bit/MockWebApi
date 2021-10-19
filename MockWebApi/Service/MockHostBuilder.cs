466d4f403b95fe1a612fb24132abc8e46ae848a1
Marco Ruether <mruether@mnemonic-bit.com>
2021-10-19 22:17:12 +0200

    [MWA-3] refactored, added MockService

---
+++ MockWebApi/Service/MockHostBuilder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MockWebApi.Extension;
+using Serilog;
+using Serilog.Filters;
+
+namespace MockWebApi.Service
+{
+    /// <summary>
+    /// Provides a host builder for creating the MockService.
+    /// </summary>
+    public class MockHostBuilder
+    {
+
+        public static IHostBuilder Create(string[] args)
+        {
+            if (args == null)
+            {
+                args = new string[] { };
+            }
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+                .CreateLogger();
+
+            return Host
+                .CreateDefaultBuilder(args)
+                .ConfigureLogging(logBuilder =>
+                {
+                    logBuilder.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseEnvironment("Development")
+                        .SetupMockWebApi();
+                });
+        }
+
+    }
+}
---
+++ MockWebApi/Service/MockService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MockWebApi.Service
+{
+    /// <summary>
+    /// The MockService is the component which represents an instance
+    /// of the MockWebApi. Since this mock-server can run multiple
+    /// instances at the same time, we encapsulated this in a class
+    /// of its own.
+    /// </summary>
+    public class MockService
+    {
+
+        private readonly IHostBuilder _hostBuilder;
+
+        public MockService(IHostBuilder hostBuilder)
+        {
+            _hostBuilder = hostBuilder;
+        }
+
+        public async Task StartServiceAsync(CancellationToken cancellationToken = default)
+        {
+            await _hostBuilder
+                .Build()
+                .RunAsync(cancellationToken);
+        }
+
+    }
+}
