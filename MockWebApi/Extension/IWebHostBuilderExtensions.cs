c491111e41594179c7515e4fe1568793a826376d
mruether <mruether@mnemonic-bit.com>
2021-10-25 20:01:14 +0200

    [MWA-4] refactored

--- MockWebApi/Extension/IWebHostBuilderExtensions.cs
+++ MockWebApi/Extension/IWebHostBuilderExtensions.cs
@@ -17,5 +17,15 @@ namespace MockWebApi.Extension
             return webHostBuilder;
         }
 
+        public static IWebHostBuilder SetupMockWebApiService(this IWebHostBuilder webHostBuilder)
+        {
+            webHostBuilder
+                .UseStartup<Startup>()
+                .UseUrls("https://0.0.0.0:6001;http://0.0.0.0:6000")
+                .UseSerilog();
+
+            return webHostBuilder;
+        }
+
     }
 }
--- MockWebApi/Program.cs
+++ MockWebApi/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MockWebApi.Extension;
 using MockWebApi.Service;
+using Serilog;
+using Serilog.Filters;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,10 +15,43 @@ namespace MockWebApi
 
         public static async Task Main(string[] args)
         {
+            //TODO: use this code to fire-up a mock-web-api from the main contraoller
+            /*
             MockService mockService = new MockService(
                 MockHostBuilder.Create(args));
 
             mockService.StartService();
+            */
+
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            if (args == null)
+            {
+                args = new string[] { };
+            }
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+                .CreateLogger();
+
+            return Host
+                .CreateDefaultBuilder(args)
+                .ConfigureLogging(logBuilder =>
+                {
+                    logBuilder.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseEnvironment("Development")
+                        .SetupMockWebApiService();
+                });
         }
 
     }
