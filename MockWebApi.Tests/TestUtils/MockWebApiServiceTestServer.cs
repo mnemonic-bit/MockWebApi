ef4f9976556922c18fece5285d11ec164494d5f5
Marco Ruether <mruether@mnemonic-bit.com>
2021-11-11 22:05:59 +0100

    [MWA-4] refactored service startup methods

---
+++ MockWebApi.Tests/TestUtils/MockWebApiServiceTestServer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using MockWebApi.Configuration;
+using MockWebApi.Extension;
+using System.Net.Http;
+
+namespace MockWebApi.Tests.TestUtils
+{
+    internal class MockWebApiServiceTestServer
+    {
+
+        private readonly TestServer _testServer;
+        private ServiceConfigurationProxy _serviceConfigurationProxy;
+
+        internal MockWebApiServiceTestServer(IServiceConfiguration serviceConfiguration)
+        {
+            _serviceConfigurationProxy = new ServiceConfigurationProxy(serviceConfiguration);
+            _testServer = CreateTestServer();
+        }
+
+        internal HttpClient CreateHttpClient()
+        {
+            return _testServer.CreateClient();
+        }
+
+        internal HttpMessageHandler CreateHttpMessageHandler()
+        {
+            return _testServer.CreateHandler();
+        }
+
+        private TestServer CreateTestServer()
+        {
+            IWebHostBuilder hostBuilder = new WebHostBuilder()
+                .SetupMockWebApiService()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<IServiceConfiguration>(_serviceConfigurationProxy);
+                });
+
+            TestServer testServer = new TestServer(hostBuilder);
+
+            return testServer;
+        }
+
+    }
+}
--- MockWebApi/Program.cs
+++ MockWebApi/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using MockWebApi.Extension;
-using MockWebApi.Service;
-using Serilog;
-using Serilog.Filters;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MockWebApi
@@ -13,7 +8,7 @@ namespace MockWebApi
     public class Program
     {
 
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             CreateHostBuilder(args)
                 .Build()
@@ -27,22 +22,13 @@ namespace MockWebApi
                 args = new string[] { };
             }
 
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .Filter.ByExcluding(Matching.FromSource("Microsoft"))
-                .CreateLogger();
-
             return Host
                 .CreateDefaultBuilder(args)
-                .ConfigureLogging(logBuilder =>
-                {
-                    logBuilder.AddConsole();
-                })
+                .ConfigureLogging()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
                         .UseEnvironment("Development")
-                        .UseUrls("http://0.0.0.0:6000") //TODO make this dynamic
                         .SetupMockWebApiService();
                 });
         }
