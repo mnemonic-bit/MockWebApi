e336c6cd685d081d5fa4b724cd3162e4ad22781c
mruether <mruether@mnemonic-bit.com>
2021-11-20 12:57:33 +0100

    [MWA-4] bugfix of template engine

--- MockWebApi/Templating/TemplateParser.cs
+++ MockWebApi/Templating/TemplateParser.cs
@@ -10,27 +10,25 @@ namespace MockWebApi.Templating
 
         public Template Parse(string text)
         {
-            (string templateText, string[] parameters) = ParseIt(text);
+            Fragment[] fragments = ParseIt(text);
 
             Template template = new Template()
             {
-                FormatString = templateText,
-                Parameters = parameters
+                Fragments = fragments
             };
 
             return template;
         }
 
-        private (string, string[]) ParseIt(string text)
+        private Fragment[] ParseIt(string text)
         {
-            List<string> parameters = new List<string>();
-            string newText = "";
+            List<Fragment> fragments = new List<Fragment>();
 
             int tempEndIndex = 0;
             int tempStartIndex = text.IndexOf("{{");
             while (tempStartIndex >= 0)
             {
-                newText += text.Substring(tempEndIndex, tempStartIndex - tempEndIndex); // start and end are switch here, because we use the former end as a start.
+                fragments.Add(new StringFragment(text.Substring(tempEndIndex, tempStartIndex - tempEndIndex)));
 
                 tempEndIndex = text.IndexOf("}}", tempStartIndex);
 
@@ -39,18 +37,16 @@ namespace MockWebApi.Templating
                     throw new MalformedTemplateException($"There is an opening double-brace ({{{{) at position { tempStartIndex } but no corresponding closing double brance (}}}})");
                 }
 
-                newText += $"{{{ parameters.Count }}}";
-
-                string parameter = text.Substring(tempStartIndex + 2, tempEndIndex - tempStartIndex - 2);
-                parameters.Add(parameter.Trim());
+                string script = text.Substring(tempStartIndex + 2, tempEndIndex - tempStartIndex - 2);
+                fragments.Add(new ScriptFragment(script.Trim()));
 
                 tempEndIndex += 2;
                 tempStartIndex = text.IndexOf("{{", tempEndIndex);
             }
 
-            newText += text.Substring(tempEndIndex, text.Length - tempEndIndex); // add the rest after the last occurance of the double-brace to the format string
+            fragments.Add(new StringFragment(text.Substring(tempEndIndex, text.Length - tempEndIndex)));
 
-            return (newText, parameters.ToArray());
+            return fragments.ToArray();
         }
 
     }
